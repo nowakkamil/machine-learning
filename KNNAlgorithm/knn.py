070862330371ee1acd6616872a05276c74962b13
nowakkamil <nowakkamil@yahoo.com>
2018-10-28 23:22:29 +0100

    Change formatting of the output and parameters of the validation methods

--- KNNAlgorithm/knn.py
+++ KNNAlgorithm/knn.py
@@ -9,6 +9,7 @@ import os
 
 
 class Sample():
+
     def __init__(self, dist, label):
         self.dist = dist
         self.label = label
@@ -18,12 +19,13 @@ class Sample():
 
 
 class KNN():
+
     euclidean = distance.euclidean
     manhattan = distance.cityblock
 
     def __init__(self, training_data, k=3, metrics=euclidean):
         try:
-            KNN.validate_parameters(self, k)
+            KNN.validate_parameters(k)
         except InvalidValueOfArgumentException as e:
             print(e.message)
             os._exit(1)
@@ -39,32 +41,37 @@ class KNN():
         self.target = list(squeeze(self.target))
         self.k = k
         self.metrics = metrics
-    
-    @staticmethod
-    def validate_parameters(self, k):
-        if k <= 0:
-            raise InvalidValueOfArgumentException("Value of k should be greater than 0")
 
     @staticmethod
-    def validate_equal_length_of_containers(self, first_container, second_container):
+    def validate_parameters(k):
+        if k <= 0:
+            raise InvalidValueOfArgumentException(
+                "Value of k should be greater than 0")
+
+    @staticmethod
+    def validate_equal_length_of_containers(first_container, second_container):
         if len(first_container) is not len(second_container):
             raise ArgumentsNotEqualException(
-                type(first_container), "Lengths of the containers of type: {} should be equal".format(type(first_container))
-                )
+                type(first_container), "Lengths of the containers of type: {} should be equal".format(
+                    type(first_container))
+            )
 
     @staticmethod
     def most_common(k, dist_vector):
         common_labels = dict()
+
         for i, dist in enumerate(dist_vector):
             common_labels.setdefault(dist.label, 0)
             common_labels[dist.label] += 1
             if i >= k - 1:
                 break
+
         return sorted(common_labels.items(), key=o.itemgetter(1))[0][0]
 
     @staticmethod
     def count_score(predictions, labels):
         score = 0
+
         for i, p in enumerate(predictions):
             if p == labels[i]:
                 score += 1
@@ -81,6 +88,7 @@ class KNN():
         self.std_sc.fit(new_data)
         new_data = self.std_sc.transform(new_data)
         result = []
+
         for unknown in new_data:
             y = []
             for i, sample in enumerate(self.data):
@@ -88,13 +96,14 @@ class KNN():
             y.sort(key=o.attrgetter("dist"))
             prediction = KNN.most_common(self.k, y)
             result.append(prediction)
+            
         return result
 
     def score(self, test_data, labels):
         result = self.predict(test_data)
 
         try:
-            KNN.validate_equal_length_of_containers(self, test_data, labels)
+            KNN.validate_equal_length_of_containers(test_data, labels)
         except ArgumentsNotEqualException as e:
             print(e.message)
             os._exit(1)
--- KNNAlgorithm/test.py
+++ KNNAlgorithm/test.py
@@ -2,6 +2,7 @@
 
 from knn import KNN
 from numpy import array, array_split
+import unittest
 import pandas as pd
 
 training_data = array(pd.read_csv("iris.data.learning", header=None))
@@ -11,5 +12,5 @@ new_data, labels = array_split(array(pd.read_csv("iris.data.test", header=None))
 for i in range(30, 1, -1):
     knn.change_k(i)
     print("k = {} => ".format(str(i).rjust(2, ' ')), end="")
-    print(f'{knn.score(new_data, labels):3.2f}')
+    print(f'{knn.score(new_data, labels):1.3f}')
 
